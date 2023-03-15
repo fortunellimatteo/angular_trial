1e0330435b5dcefe8241b9abbdfe2d823eb821d6
Utente <Utente@Home-PC.station>
2023-03-15 19:34:40 +0100

    fix @Path for query

--- reservationSystem/src/main/java/com/ap/reservationSystem/ReservationRepository.java
+++ reservationSystem/src/main/java/com/ap/reservationSystem/ReservationRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ap.reservationSystem.dto.ReservationDTO;
 
@@ -15,18 +16,18 @@ public interface ReservationRepository extends CrudRepository<Reservation,Long>{
 
 	  @Query("SELECT new com.ap.reservationSystem.dto.ReservationDTO(idReservation, nameReservation, dateReservation, "
 	  		+ "hoursReservation, fieldReservation) FROM Reservation WHERE nameReservation = :name")
-	  public List<ReservationDTO> getReservationsByNameReservation(String name);
+	  public List<ReservationDTO> getReservationsByNameReservation(@Param("name") String name);
 	 
 	  @Modifying
 	  @Transactional
 	  @Query("DELETE FROM Reservation WHERE nameReservation = :name AND dateReservation = :date AND hoursReservation = :hour")
-	  public void deleteReservationByNameAndDate(String name, Date date, Long hour);
+	  public void deleteReservationByNameAndDate(@Param("name") String name, @Param("date") Date date, @Param("hour") Long hour);
 	  
 	  @Query("SELECT new com.ap.reservationSystem.dto.ReservationDTO(idReservation, nameReservation, dateReservation, "
 		  		+ "hoursReservation, fieldReservation) FROM Reservation WHERE dateReservation = :date")
-	  public List<ReservationDTO> getReservationsOfADay(Date date);
+	  public List<ReservationDTO> getReservationsOfADay(@Param("date") Date date);
 
 	  @Query("SELECT new com.ap.reservationSystem.dto.ReservationDTO(idReservation, nameReservation, dateReservation, "
 		  		+ "hoursReservation, fieldReservation) FROM Reservation WHERE dateReservation BETWEEN :dateFrom AND :dateTo")
-	  public List<ReservationDTO> getReservationsBetweenDates(Date dateFrom, Date dateTo);
+	  public List<ReservationDTO> getReservationsBetweenDates(@Param("dateFrom") Date dateFrom, @Param("dateTo") Date dateTo);
 }
